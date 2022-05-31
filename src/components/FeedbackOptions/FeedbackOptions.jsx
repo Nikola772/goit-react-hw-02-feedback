b1efd452a419730a22464ecb8c6af9cf7dcc4e9e
Коля <niklutenko@gmail.com>
2022-05-31 08:04:15 +0300

    reading package.json

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -2,6 +2,7 @@
 
 export const FeedbackOptions = ({hendleClickGood, hendleClickNeutral, hendleClickBad}) => {
     return (
+        
         <div>
             <button type="button" onClick={hendleClickGood} >Good</button>
             <button type="button" onClick={hendleClickNeutral}>Neutral</button>
